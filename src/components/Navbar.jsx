a9c77b6014b01d84a865558affb2acd7d24ebf72
Yonatan Haddish <yonihailshn@gmail.com>
2024-05-30 17:21:13 -0400

    navbar structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDesktop, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [searchResult, setSearchRestult] = useState("");
+
+  function handleSearch(e) {
+    setSearchRestult(e.target.value);
+  }
+
   return (
     <>
       <div className="navbarBox">
@@ -11,16 +17,16 @@ function Navbar() {
           <FontAwesomeIcon icon={faDesktop} />
         </div>
         <div className="navbarListBox">
-          <div className="homeBox">
+          <div className="homeBox" id="navList">
             <Link to="/">Home</Link>
           </div>
-          <div className="moviesBox">
+          <div className="moviesBox" id="navList">
             <Link to="/movies">Movies</Link>
           </div>
-          <div className="tvShowsBox">
+          <div className="tvShowsBox" id="navList">
             <Link to="/tv-shows">TV Shows</Link>
           </div>
-          <div className="browseByLangBox">
+          <div className="browseByLangBox" id="navList">
             <Link to="/browseByLanguages">Browse By Languages</Link>
           </div>
         </div>
@@ -28,7 +34,15 @@ function Navbar() {
           <div className="searchIcon">
             <FontAwesomeIcon icon={faSearch} />
           </div>
-          <div className="iconAndTextField"></div>
+          <div className="iconAndTextFieldBox">
+            <div className="icon">
+              {" "}
+              <FontAwesomeIcon icon={faSearch} />
+            </div>
+            <div className="searchField">
+              <textarea value={searchResult} onChange={handleSearch} />
+            </div>
+          </div>
         </div>
       </div>
     </>
